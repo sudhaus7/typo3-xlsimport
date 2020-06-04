b646166dfc9b4c839fdcf9120b1727cccc0c4095
Frank Berger <fberger@sudhaus7.de>
2020-06-04 16:57:35 +0200

    Fix missing dependencies when extension is installed in manual / classic mode through the TYPO3 Extension Manager

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
 	'title' => '(Sudhaus7) XLS Importer',
 	'description' => 'A simple importer for table shown content in the database',
 	'category' => 'module',
-	'version' => '1.0.4',
+	'version' => '1.0.5',
 	'state' => 'stable',
 	'uploadfolder' => 1,
 	'clearcacheonload' => 0,
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
 	'author_company' => 'Sudhaus7, ein Label der B-Factor GmbH',
 	'constraints' => [
 		'depends' => [
-		    'typo3' => '6.2.0-10.4.99'
+		    'typo3' => '7.6.0-10.4.99'
         ],
 		'conflicts' => [
         ],
@@ -22,7 +22,15 @@ $EM_CONF[$_EXTKEY] = [
     ],
     'autoload' => [
         'psr-4' => [
-            'SUDHAUS7\\Xlsimport\\' => 'Classes'
+            'SUDHAUS7\\Xlsimport\\' => 'Classes',
+            'ZipStream\\' => 'vendor/maennchen/zipstream-php/src',
+            'Symfony\\Polyfill\\Mbstring\\' => 'vendor/symfony/polyfill-mbstring',
+            'Psr\\SimpleCache\\' => 'vendor/psr/simple-cache/src',
+            'Psr\\Http\\Message\\' => 'vendor/psr/http-message/src',
+            'PhpOffice\\PhpSpreadsheet\\' => 'vendor/phpoffice/phpspreadsheet/src/PhpSpreadsheet',
+            'MyCLabs\\Enum\\' => 'vendor/myclabs/php-enum/src',
+            'Matrix\\' => 'vendor/markbaker/matrix/classes/src',
+            'Complex\\' => 'vendor/markbaker/complex/classes/src',
         ]
     ],
 ];
