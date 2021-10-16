0098ab22edf9fd159099ac8b5d9f4bf6cfdf6d64
Peter Kraume <peter.kraume@gmx.de>
2021-10-16 09:07:03 +0200

    [TASK] Update TYPO3_MODE check

--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,6 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
-}
+defined('TYPO3_MODE') || die();
+
 (function () {
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
         'Xlsimport',
