7999a1001759a3353421d0d9bb76e70b53ca19fa
Jason <jreynolds@b-factor.de>
2023-07-13 12:27:25 +0200

    Composer Update for Typo3 v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF['xlsimport'] = [
     'title' => '(Sudhaus7) XLS Importer',
     'description' => 'A simple importer to import data into the database',
     'category' => 'module',
-    'version' => '4.0.0',
+    'version' => '5.0.0',
     'state' => 'stable',
     'uploadfolder' => 1,
     'clearcacheonload' => 0,
@@ -13,7 +13,7 @@ $EM_CONF['xlsimport'] = [
     'author_company' => 'Sudhaus7, ein Label der B-Factor GmbH',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '12.0.0-12.9.99',
         ],
         'conflicts' => [
         ],
